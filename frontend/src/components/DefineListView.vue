2de3c7e5f492bee2413599229a4d1e7610a76126
mcbauman <m.c.baumann@icloud.com>
2023-05-10 20:12:36 +0200

    workingOnOrder

--- frontend/src/components/DefineListView.vue
+++ frontend/src/components/DefineListView.vue
@@ -4,9 +4,12 @@ import { ref } from 'vue';
 const keys=useKeyResponseStore()
 const changesHelper=ref([])
 
-function submitfunction(){
+const test =ref()
+
+function submitfunction(id:string,value:any){
+    changesHelper.value.push(value)
     console.log(changesHelper.value);
-    // changesHelper.value.forEach((e) => console.log(Object.keys(e)))
+    //changesHelper.value.forEach((e) => console.log(Object.keys(e)))
     // changesHelper.value.forEach((e) => keys.updateKey(Object.keys(e)[0],{index2:Object.values(e)[0]}))
 }
 </script>
@@ -24,10 +27,12 @@ function submitfunction(){
                 <td>{{ key.name }}</td>
                 <td>{{ key.type }}</td>
                 <td>
-                    <input type="number" v-model="changesHelper[key._id]">
+                    <input type="number" v-model="changesHelper[key._id]" @change="submitfunction(key._id, changesHelper[key._id])">
                 </td>
             </tr>
+            <p>{{ changesHelper }}</p>
+            <p v-for="item in changesHelper"><strong> {{item}} </strong></p>
         </table>
-        <button class="submit" @click="submitfunction">Safe</button>
+        <!-- <button class="submit" @click="submitfunction">Safe</button> -->
     </section>
 </template>
