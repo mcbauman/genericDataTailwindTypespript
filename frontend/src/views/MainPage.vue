79506175fef45a3c96a5af8e525dee2ca656e6b5
mcbauman <m.c.baumann@icloud.com>
2023-05-24 09:11:12 +0200

    small improvemtns

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -1,44 +1,21 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import BaseForm from "@/components/BaseComponents/BaseForm.vue";
 import { useKeyResponseStore } from "@/stores/keyResonse";
-import type KeysInterface from "@/interfaces/KeyInterface";
 import { useValueStore } from "@/stores/ValueStore";
 
 const keys = useKeyResponseStore();
 const value = useValueStore();
-const newValue = ref({})
-
-console.log("keys.Keys",keys.Keys);
-
-// function setDefaults(){
-//   keys.Keys.forEach(key=>{
-//   if(key.type==="Object"){
-//     console.log("if")
-//     let helper={}
-//     key.objectEntries.forEach(objectEntrie=>{
-//     helper[objectEntrie.name]=""
-//     console.log(helper);
-//     })
-//     newValue.value[key.name]=helper
-//   }else{
-//   newValue.value[key.name]="placeholder"
-//   }
-// })
-// console.log("NEWVALUE",newValue.value);
-// }
-
-
 
 function storenNewValue(){
     console.log(value.Values);
+    value.storeNewValue()
+    value.Values={}
 }
 </script>
 
 <template>
   <div class="text-white">DEFINE KEYS</div>
   <section id="DefineKeys">DEFINE KEYS
-    <!-- <button @click="setDefaults">SET DEFAULT VALUES</button> -->
   </section>
   <div class="text-white">ENTERVALUES</div>
   <section id="EnterValues">
@@ -48,4 +25,4 @@ function storenNewValue(){
   <div class="text-white">SHOWVALUES</div>
   <section id="ShowValues">SHWOW-VALUES</section>
 </template>
-<!-- JUST CA COMMENT -->
+
